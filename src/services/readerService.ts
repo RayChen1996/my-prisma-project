354e6f584269d67311f2e215093c31272e566330
RayChen1996 <53119155+RayChen1996@users.noreply.github.com>
2024-06-14 21:40:01 +0800

    Update readerService.ts

--- src/services/readerService.ts
+++ src/services/readerService.ts
@@ -1,15 +1,27 @@
 import prisma from "../../prisma/client";
 import { Reader } from "../models/reader";
+interface ReaderData {
+  name: string;
+  email: string;
+}
 
-const createReader = async (readerData: Reader) => {
-  const reader = await prisma.reader.create({
-    data: readerData,
-  });
-  return reader;
-};
+// export const createReader = async (data: ReaderData) => {
+//   const { name, email } = data;
+//   try {
+//     const reader = await prisma.reader.create({
+//       data: {
+
+//       },
+//     });
+//     return reader;
+//   } catch (error) {
+//     console.error("Error creating reader:", error);
+//     throw new Error("Failed to create reader");
+//   }
+// };
 
 // 實作其他 CRUD 操作
 
 export default {
-  createReader,
+  // createReader,
 };
